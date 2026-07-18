8d05eb3041627d185e8758d5622016fe0359b966
ByKaoS34 <63543637+ByKaoS34@users.noreply.github.com>
2026-07-18 15:57:37 +0300

    Update class-llms.php

--- includes/class-llms.php
+++ includes/class-llms.php
@@ -4,74 +4,41 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
+
 class AVS_LLMS {
 
 
-    public static function create_llms_file() {
+    public function __construct() {
 
 
-        $content = "# " . get_option('avs_site_name', get_bloginfo('name')) . "\n\n";
-
-
-        $content .= "## About\n";
-
-        $content .= get_option(
-            'avs_description',
-            get_bloginfo('description')
+        add_action(
+            'init',
+            array($this,'generate')
         );
 
-        $content .= "\n\n";
+
+    }
 
 
-        $phone = get_option('avs_phone');
 
-        if ($phone) {
-
-            $content .= "## Contact\n";
-            $content .= "Phone: " . $phone . "\n\n";
-
-        }
+    public function generate(){
 
 
-        $whatsapp = get_option('avs_whatsapp');
-
-        if ($whatsapp) {
-
-            $content .= "WhatsApp: " . $whatsapp . "\n\n";
-
-        }
+        $generator = new AVS_Generator();
 
 
-        $keywords = get_option('avs_keywords');
+        $content = $generator->generate();
 
-        if ($keywords) {
-
-            $content .= "## Keywords\n";
-            $content .= $keywords . "\n\n";
-
-        }
-
-
-        $content .= "## Content\n\n";
-
-
-        $posts = get_posts(array(
-            'numberposts' => 20,
-            'post_status' => 'publish'
-        ));
-
-
-        foreach ($posts as $post) {
-
-            $content .= "- " . get_the_title($post->ID) . "\n";
-
-        }
 
 
         file_put_contents(
+
             ABSPATH . 'llms.txt',
+
             $content
+
         );
+
 
     }
 
